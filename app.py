b2590664bfb0f90db7ce55828e176d4cfb67edc0
KR_Hound <51309303+KRhound@users.noreply.github.com>
2024-05-07 16:38:12 +0900

    Implement download, upload functions, implement edit and delete functions
    Additional login access controls

--- app.py
+++ app.py
@@ -35,19 +35,28 @@ def filename_sha_512_hash(data):
     hashed_data = hashlib.sha512(encoded_data).hexdigest()
     return hashed_data
 
-
 conn = sqlite3.connect('web.db', check_same_thread=False)
 cursor = conn.cursor()
 
 # 메인 페이지
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('profile.html')
-
 # 프로필 페이지
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
 @app.route('/profile', methods=['GET'])
 def profile():
     return render_template('profile.html')
+
+# 포트폴리오 페이지
+@app.route('/portfolio', methods=['GET'])
+def portfolio():
+    return render_template('portfolio.html')
+
+# 에러 페이지
+@app.route('/error', methods=['GET'])
+def error():
+    error = request.args.get('error', None, type=str)
+    if error is not None:
+        return render_template('error.html', error=error)
 
 # 게시판 페이지
 @app.route('/board', methods=['GET'])
@@ -55,62 +64,117 @@ def board():
     try:
         status = 1
         select_query = """
-        SELECT id, title, username, modification_date FROM boards WHERE status = ?
+        SELECT id, title, username, modification_date FROM boards WHERE status = ? ORDER BY modification_date DESC;
         """
         cursor.execute(select_query, (status, ))
         posts = cursor.fetchall()
         return render_template('board.html', posts=posts)
     except sqlite3.InternalError:
-        msg = "문제 발생"
-        return render_template('profile.html', msg=msg)
+        msg = "Bulletin board loading failure."
+        return redirect(url_for('error', error=error))
 
 # 게시글 보기 페이지
 @app.route('/view_post', methods=['GET'])
 def view_post():
-    try:
-        id = request.args.get('id', '', type=int)
-        status = 1
-        select_query = """
-        SELECT id, title, username, content, real_filename, modification_date FROM boards WHERE id = ? AND status = ?
-        """
-        cursor.execute(select_query, (id, status))
-        post = cursor.fetchone()
-        return render_template('view_post.html', post=post)
-    except sqlite3.IntegrityError:
-        msg = "게시글 없음"
-        return render_template('board.html', msg=msg)
+    if request.method == 'GET':
+        try:
+            if 'username' not in session:
+                error = "Login required"
+                return redirect(url_for('error', error=error))
+            U_id = session['id']
+            username = session['username']
+            authority = session['authority']
+            id = request.args.get('id', '', type=int)
+            status = 1
+            select_query = """
+            SELECT id, title, username, content, real_filename, modification_date FROM boards WHERE id = ? AND status = ?
+            """
+            cursor.execute(select_query, (id, status))
+            post = cursor.fetchone()
+            return render_template('view_post.html', post=post)
+        except sqlite3.IntegrityError:
+            error = "Unusual approach."
+            return redirect(url_for('error', error=error))
     
 # 게시글 수정 페이지
-# @app.route('/update_post', methods=['GET'])
-# def update_post():
-#     if request.method == "GET":
-#         id = request.args.get('id', '', type=int)
-        
-#         return render_template('edit_post.html', post=post)
-#     elif request.method == 'POST':
-#         try:
+@app.route('/update_post', methods=['GET', 'POST'])
+def update_post():
+    if request.method == "GET":
+        try:
+            if 'username' not in session:
+                error = "Login required"
+                return redirect(url_for('error', error=error))
+            id = request.args.get('id', '', type=int)
+            U_id = session['id']
+            username = session['username']
+            authority = session['authority']
+            status = 1
+            select_query = """
+            SELECT id, title, username, content, real_filename FROM boards WHERE id = ? AND U_id = ? AND status = ?
+            """
+            cursor.execute(select_query, (id, U_id, status))
+            post = cursor.fetchone()
+            return render_template('edit_post.html', post=post)
+        except:
+            error = "Failed to edit post."
+            return redirect(url_for('error', error=error)) 
+    elif request.method == 'POST':
+        try:
+            if 'username' not in session:
+                error = "Login required"
+                return redirect(url_for('error', error=error))
+            id = request.args.get('id', '', type=int)
+            title = request.form['title']
+            content = request.form['content']
+            U_id = session['id']
+            username = session['username']
+            authority = session['authority']
+            status = 1
             
-#             status = 1
-#             update_query = """
-#             INSERT INTO boards (U_id, username, title, content, real_filename, hash_filename, status) 
-#             VALUES (?, ?, ?, ?, ?, ?, ?)
-#             """
-#             cursor.execute(update_query, (id, status))
-#             conn.commit()
-#             select_query = """
-#             SELECT id, title, username, content, real_filename, modification_date FROM boards WHERE id = ? AND status = ?
-#             """
-#             cursor.execute(select_query, (id, status))
-#             post = cursor.fetchone()
-#             return render_template('view_post.html', post=post)
-#         except:
-#             return render_template('board.html')
+            # 파일 업로드 처리
+            file = request.files['file']
+            if file and allowed_file(file.filename):
+                # 파일 크기 제한 설정 (최대 1MB)
+                if len(file.read()) > 1024*1024*30:
+                    msg = "파일 크기가 너무 큽니다. 최대 30MB까지 업로드 가능합니다."
+                    return render_template('create_post.html', msg=msg)
+                file.seek(0)  # 파일을 다시 읽을 수 있도록 커서 위치를 처음으로 이동시킴
+                real_filename = file.filename
+                hash_filename = filename_sha_512_hash(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], hash_filename))
+            else:
+                real_filename = None
+                hash_filename = None
+            
+            if real_filename is None and hash_filename is None:
+                update_query = """
+                UPDATE boards 
+                SET title = ?, content = ?
+                WHERE id = ? AND username = ? AND status = ?
+                """
+                cursor.execute(update_query, (title, content, id, username, status))
+                conn.commit()
+            else:
+                update_query = """
+                UPDATE boards 
+                SET title = ?, content = ?, real_filename = ?, hash_filename = ?
+                WHERE id = ? AND username = ? AND status = ?
+                """
+                cursor.execute(update_query, (title, content, real_filename, hash_filename, id, username, status))
+                conn.commit()
+            return redirect(url_for('view_post', id=id))
+        except:
+            error = "Failed to edit post."
+            return redirect(url_for('error', error=error))
 
 # 게시글 삭제 페이지
 @app.route('/delete_post', methods=['GET'])
 def delete_post():
     if request.method == 'GET':
         try:
+            if 'username' not in session:
+                error = "Login required"
+                return redirect(url_for('error', error=error))
             id = request.args.get('id', '', type=int)
             status = 0
             update_query = """
@@ -122,16 +186,22 @@ def delete_post():
             conn.commit()
             return redirect(url_for('board'))
         except:
-            msg = "게시글 삭제 실패"
-            return render_template('board.html', msg=msg)
+            error = "Failed to delete post."
+            return redirect(url_for('error', error=error))
 
 # 게시글 생성 기능
 @app.route('/create_post', methods=['GET', 'POST'])
 def create_post():
     if request.method == 'GET':
+        if 'username' not in session:
+                error = "Login required"
+                return redirect(url_for('error', error=error))
         return render_template('create_post.html')
     elif request.method == 'POST':
         try:
+            if 'username' not in session:
+                error = "Login required"
+                return redirect(url_for('error', error=error))
             title = request.form['title']
             content = request.form['content']
             U_id = session['id']
@@ -163,13 +233,16 @@ def create_post():
             conn.commit()
             return redirect(url_for('board'))
         except sqlite3.IntegrityError:
-            msg = "작성 실패"
-            return render_template('create_post.html', msg=msg)
+            error = "Creation failed."
+            return redirect(url_for('error', error=error))
 
 # 다운로드 페이지
 @app.route('/download/<id>', methods=['GET'])
 def download(id):
     try:
+        if 'username' not in session:
+            error = "Login required"
+            return redirect(url_for('error', error=error))
         status = 1
         select_query = """
         SELECT real_filename, hash_filename FROM boards WHERE id = ? AND status = ?
@@ -186,22 +259,24 @@ def download(id):
         if os.path.exists(file_path):
             return send_file(file_path, download_name=real_filename, as_attachment=True)
         else:
-            return render_template('error.html', error='File not found')
+            error='File not found'
+            return redirect(url_for('error', error=error))
     except Exception as e:
         return str(e)
-
-# 포트폴리오 페이지
-@app.route('/portfolio', methods=['GET'])
-def portfolio():
-    return render_template('portfolio.html')
 
 # CONTACT ME 페이지
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'GET':
+        if 'username' not in session:
+            error = "Login required"
+            return redirect(url_for('error', error=error))
         return render_template('contact.html')
     elif request.method == 'POST':
         try:
+            if 'username' not in session:
+                error = "Login required"
+                return redirect(url_for('error', error=error))
             U_id = session['id']
             username = session['username']
             title = request.form['title']
@@ -218,11 +293,11 @@ def contact():
                 conn.commit()
                 return render_template('contact_list.html')
             else:
-                msg = "입력값 없음"
-                return render_template('contact.html', msg=msg)
+                error = "No input."
+                return redirect(url_for('error', error=error))
         except sqlite3.IntegrityError:
-            msg = "작성 실패"
-            return render_template('contact.html', msg=msg)
+            error = "Creation failed."
+            return redirect(url_for('error', error=error))
 
 # 로그인 페이지
 @app.route('/login', methods=['GET', 'POST'])
@@ -244,13 +319,13 @@ def login():
                 session['id'] = user[0]
                 session['username'] = user[1]
                 session['authority'] = user[2]
-                return render_template('/profile.html')
+                return redirect(url_for('profile'))
             else:
-                msg = "계정 불일치"
-                return render_template('login.html', msg=msg)
+                error = "Account mismatch."
+                return redirect(url_for('error', error=error))
         except sqlite3.IntegrityError:
-            msg = "로그인 실패"
-            return render_template('login.html', msg=msg)
+            error = "Login failed"
+            return redirect(url_for('error', error=error))
 
 # 회원가입 페이지
 @app.route('/register', methods=['GET', 'POST'])
@@ -278,6 +353,7 @@ def register():
                 """
                 cursor.execute(insert_query, (fullname, username, id, password, gender, phone, email, authority, status))
                 conn.commit()
+                msg = "Membership registration successful!"
                 return render_template('/login.html', msg=msg)
             else:
                 msg = "비밀번호 불일치"
@@ -292,10 +368,15 @@ def logout():
     if request.method == 'GET':
         try:
             session.clear()  # 세션에서 사용자 정보 삭제
-            return render_template('/profile.html', msg=msg)
+            return render_template('/profile.html')
         except:
-            msg = "로그아웃 실패"
+            msg = "Logout failed."
             return render_template('/profile.html', msg=msg)
+
+# 관리자 페이지 구현  
+@app.route('/ed9465dea93268e8', methods=['GET'])
+def admin():
+    return render_template('krhound.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- secret_key.py
+++ secret_key.py
@@ -1,4 +1,4 @@
 import secrets
 
-secret_key = secrets.token_hex(128)
+secret_key = secrets.token_hex(8)
 print(secret_key)
